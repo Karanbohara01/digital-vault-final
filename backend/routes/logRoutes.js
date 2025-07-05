253c1d637807443a87932d57371ee279a0765b6c
Karanbohara01 <karanbohara216@gmail.com>
2025-07-05 12:50:07 +0545

    right click disabled and drag and drop disabled

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -1,16 +1,4 @@
-// // backend/routes/logRoutes.js
 
-// const express = require('express');
-// const router = express.Router();
-// const { getAllLogs } = require('../controllers/logController');
-// const { protect, authorize } = require('../middleware/authMiddleware');
-
-// // This route is protected and can only be accessed by an admin
-// router.route('/').get(protect, authorize('admin'), getAllLogs);
-
-// module.exports = router;
-
-// backend/routes/logRoutes.js
 
 const express = require('express');
 const router = express.Router();
