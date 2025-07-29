6c7b2b21b62b1228de87ad8aa125595e64a6a2e8
Karanbohara01 <karanbohara216@gmail.com>
2025-07-29 15:21:45 +0545

    rate limitter backend created

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,8 +3,7 @@
 const rateLimit = require('express-rate-limit');
 const { logActivity } = require('../services/logService');
 
-// Limiter for sensitive actions like login and password updates
-// Allows 10 requests per 15 minutes
+
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 10, // Limit each IP to 10 requests per windowMs
@@ -14,17 +13,15 @@ const authLimiter = rateLimit({
         logActivity(null, 'AUTH_RATE_LIMIT_EXCEEDED', 'fatal', { ipAddress: req.ip });
         res.status(options.statusCode).send(options.message);
     },
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    standardHeaders: true,
+    legacyHeaders: false,
 });
 
 
-// General limiter for all other API calls
-// Allows 100 requests per 15 minutes
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100,
-    message: 'Too many requests from this IP, please try again after 15 minutes',
+    message: 'Too many requests from this IP, please try again after 15 minutes!',
     handler: (req, res, next, options) => {
         logActivity(null, 'API_RATE_LIMIT_EXCEEDED', 'error', { ipAddress: req.ip });
         res.status(options.statusCode).send(options.message);
